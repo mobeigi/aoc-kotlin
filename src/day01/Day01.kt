6e80b5fd95c199f7b565b180429cc182bedc05ae
Mo Beigi <me@mobeigi.com>
2023-12-03 15:23:30 +1100

    Make object private and format with ktfmt

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -5,21 +5,23 @@ import day01.Day01.part2
 import println
 import readInput
 
-object Day01 {
+private object Day01 {
     private val digitCharArray = ('0'..'9').toList().toCharArray()
-    private val spelledDigits = listOf(
-        "one" to 1,
-        "two" to 2,
-        "three" to 3,
-        "four" to 4,
-        "five" to 5,
-        "six" to 6,
-        "seven" to 7,
-        "eight" to 8,
-        "nine" to 9,
-    )
+    private val spelledDigits =
+        listOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9,
+        )
+
     fun part1(input: List<String>): Int =
-        input.sumOf {line ->
+        input.sumOf { line ->
             val digits = getLeftAndRightDigits(line)
             check(digits.first != null && digits.second != null)
             "${digits.first!!.value}${digits.second!!.value}".toInt()
@@ -36,35 +38,31 @@ object Day01 {
             check(digits.first != null || spelledDigits.first != null)
             check(digits.second != null || spelledDigits.second != null)
 
-            val leftDigit = if (digits.first == null) {
-                spelledDigits.first!!.value
-            }
-            else if (spelledDigits.first == null) {
-                digits.first!!.value
-            }
-            else {
-                if (digits.first!!.index < spelledDigits.first!!.index) {
-                    digits.first!!.value
-                }
-                else {
+            val leftDigit =
+                if (digits.first == null) {
                     spelledDigits.first!!.value
+                } else if (spelledDigits.first == null) {
+                    digits.first!!.value
+                } else {
+                    if (digits.first!!.index < spelledDigits.first!!.index) {
+                        digits.first!!.value
+                    } else {
+                        spelledDigits.first!!.value
+                    }
                 }
-            }
 
-            val rightDigit = if (digits.second == null) {
-                spelledDigits.second!!.value
-            }
-            else if (spelledDigits.second == null) {
-                digits.second!!.value
-            }
-            else {
-                if (digits.second!!.index > spelledDigits.second!!.index) {
-                    digits.second!!.value
-                }
-                else {
+            val rightDigit =
+                if (digits.second == null) {
                     spelledDigits.second!!.value
+                } else if (spelledDigits.second == null) {
+                    digits.second!!.value
+                } else {
+                    if (digits.second!!.index > spelledDigits.second!!.index) {
+                        digits.second!!.value
+                    } else {
+                        spelledDigits.second!!.value
+                    }
                 }
-            }
 
             "${leftDigit}${rightDigit}".toInt()
         }
@@ -80,8 +78,7 @@ object Day01 {
         // Handle single digit case
         if (leftDigitIndex == -1) {
             leftDigitIndex = rightDigitIndex
-        }
-        else if (rightDigitIndex == -1) {
+        } else if (rightDigitIndex == -1) {
             rightDigitIndex = leftDigitIndex
         }
 
@@ -89,10 +86,12 @@ object Day01 {
         val rightDigit = line[rightDigitIndex]
 
         return IndexedValue(leftDigitIndex, leftDigit.digitToInt()) to
-                IndexedValue(rightDigitIndex, rightDigit.digitToInt())
+            IndexedValue(rightDigitIndex, rightDigit.digitToInt())
     }
 
-    private fun getLeftAndRightSpelledDigits(line: String): Pair<IndexedValue<Int>?, IndexedValue<Int>?> {
+    private fun getLeftAndRightSpelledDigits(
+        line: String
+    ): Pair<IndexedValue<Int>?, IndexedValue<Int>?> {
         var spelledDigitFirstIndex: IndexedValue<Int>? = null
         var spelledDigitLastIndex: IndexedValue<Int>? = null
 
@@ -101,16 +100,23 @@ object Day01 {
             val index = line.indexOf(spelledDigit)
             val lastIndex = line.lastIndexOf(spelledDigit)
 
-            if (index != -1 && (spelledDigitFirstIndex == null || index < spelledDigitFirstIndex!!.index)) {
+            if (
+                index != -1 &&
+                    (spelledDigitFirstIndex == null || index < spelledDigitFirstIndex!!.index)
+            ) {
                 spelledDigitFirstIndex = IndexedValue(index, it.second)
             }
-            if (lastIndex != -1 && (spelledDigitLastIndex == null || lastIndex > spelledDigitLastIndex!!.index)) {
+            if (
+                lastIndex != -1 &&
+                    (spelledDigitLastIndex == null || lastIndex > spelledDigitLastIndex!!.index)
+            ) {
                 spelledDigitLastIndex = IndexedValue(lastIndex, it.second)
             }
         }
         return spelledDigitFirstIndex to spelledDigitLastIndex
     }
 }
+
 fun main() {
     val input = readInput(Day01.javaClass.simpleName)
 
