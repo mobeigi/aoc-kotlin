23d46e5a33ab9e679cddd368578b505ac912af42
Mo Beigi <me@mobeigi.com>
2023-12-03 15:23:43 +1100

    Add solution for day02

---
+++ src/day02/Day02.kt
@@ -0,0 +1,92 @@
+package day02
+
+import day02.Day02.part1
+import day02.Day02.part2
+import println
+import readInput
+
+private object Day02 {
+    data class RevealSet(val red: Int = 0, val green: Int = 0, val blue: Int = 0)
+
+    data class Game(val id: Int, val revealSet: List<RevealSet>)
+
+    val MAX_CAPACITY = RevealSet(red = 12, green = 13, blue = 14)
+
+    /** Parse input into list of Game */
+    private fun parseToGames(input: List<String>): List<Game> =
+        input.map { line ->
+            val components = line.split(":", ";")
+            check(components.size > 2) // at least game identifier and 1 reveal set
+            val id = components.first().split(" ").last().toInt()
+            val revealSet =
+                components.drop(1).map {
+                    val revealSetComponents = it.split(",")
+                    var red = 0
+                    var green = 0
+                    var blue = 0
+                    revealSetComponents.forEach {
+                        val (cubeCount, cubeColour) = it.trim().split(" ")
+                        when (cubeColour) {
+                            "red" -> red = cubeCount.toInt()
+                            "green" -> green = cubeCount.toInt()
+                            "blue" -> blue = cubeCount.toInt()
+                            else -> throw IllegalArgumentException("Unknown colour $cubeColour")
+                        }
+                    }
+                    RevealSet(red = red, green = green, blue = blue)
+                }
+            Game(id, revealSet)
+        }
+
+    fun part1(input: List<String>): Int {
+        val games = parseToGames(input)
+        return games.sumOf { game ->
+            val isValidGame =
+                game.revealSet.all {
+                    val redLeft = MAX_CAPACITY.red - it.red
+                    val greenLeft = MAX_CAPACITY.green - it.green
+                    val blueLeft = MAX_CAPACITY.blue - it.blue
+                    redLeft >= 0 && greenLeft >= 0 && blueLeft >= 0
+                }
+
+            if (isValidGame) game.id else 0
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        val games = parseToGames(input)
+        return games.sumOf { game ->
+            var minRed = 0
+            var minGreen = 0
+            var minBlue = 0
+            game.revealSet.forEach {
+                if (it.red > minRed) {
+                    minRed = it.red
+                }
+                if (it.green > minGreen) {
+                    minGreen = it.green
+                }
+                if (it.blue > minBlue) {
+                    minBlue = it.blue
+                }
+            }
+
+            // compute power of the minimum set of cubes
+            minRed * minGreen * minBlue
+        }
+    }
+}
+
+fun main() {
+    val input = readInput(Day02.javaClass.simpleName)
+
+    // Test part 1
+    val part1Result = part1(input)
+    part1Result.println()
+    check(part1Result == 2101)
+
+    // Test part 2
+    val part2Result = part2(input)
+    part2Result.println()
+    check(part2Result == 58269)
+}
