f014109b0c41ab78df4678e4846bec7c9800c27e
Mo Beigi <me@mobeigi.com>
2023-12-04 21:10:37 +1100

    Add template for DayX

---
+++ src/template/DayX.kt
@@ -0,0 +1,29 @@
+package template
+import readInput
+
+/**
+ * Template for DayX
+ */
+private object DayX {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+}
+
+fun main() {
+    val input = readInput(DayX.javaClass.simpleName)
+
+    // Test part 1
+    val part1Result = DayX.part1(input)
+    println(part1Result)
+//    check(part1Result == 123456)
+
+    // Test part 2
+    val part2Result = DayX.part2(input)
+    println(part2Result)
+//    check(part2Result == 123456)
+}
